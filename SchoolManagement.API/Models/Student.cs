2bfa9981177cb1a6cbe46734a075647da9a735b8
roodev <rodrigo.fessore@gmail.com>
2024-01-25 00:46:44 -0300

    Iniciando o projeto criando o teste (Criar Aluno) e implementando a primeira versão da Model Student com as propriedades requisitadas no desafio

---
+++ SchoolManagement.API/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace SchoolManagement.API.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string RA { get; private set; }
+        public string CPF { get; private set; }
+
+        public Student(string name, string email, string ra, string cpf) 
+        {
+            Name = name;
+            Email = email;
+            RA = ra;
+            CPF = cpf;
+        }
+
+    }
+
+   
+}
---
+++ SchoolManagement.XUnit/StudentTests.cs
@@ -0,0 +1,25 @@
+using SchoolManagement.API.Models;
+
+namespace SchoolManagement.XUnit
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void CreateNewStudent()
+        {
+            var studentName = "Rodrigo Fessore";
+            var studentEmail = "rodrigo.fessore@gmail.com";
+            var studentRA = "114678601";
+            var studentCPF = "356.152.728-05";
+
+            var student = new Student(studentName, studentEmail, studentRA, studentCPF);
+
+            Assert.NotNull(student);
+            Assert.Equal(studentName, student.Name);
+            Assert.Equal(studentEmail, student.Email);
+            Assert.Equal(studentRA, student.RA);
+            Assert.Equal(studentCPF, student.CPF);
+
+        }       
+    }
+}
